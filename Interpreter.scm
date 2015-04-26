ea90f052b737e19fb5d905b8ca2a1a871278671d
James Flinn <jrf116@case.edu>
2015-04-26 15:00:43 -0400

    test 13 works

--- Interpreter.scm
+++ Interpreter.scm
@@ -390,6 +390,8 @@
 (define get-func-closure
   (lambda (name state class-env instance)
     (cond 
+      ((and (not (null? (instance-class-name instance))) (var-in-class-method? name (MVvariable (instance-class-name instance) state class-env instance))) 
+       (MVvariable name (list (class-method-env (MVvariable (instance-class-name instance) state class-env instance)) (last-layer state)) class-env instance))
       ((var-in-state? name state) (MVvariable name state class-env instance))
       ((and (pair? name) (eq? 'dot (car name))) (MVdot (caddr name) state (get-class-dot (cadr name) state class-env instance) (get-instance-dot (cadr name) state class-env instance)));(MVvariable (caddr name) (list (class-method-env (MVvariable (cadr name) state class-env instance)) (last-layer state)) class-env instance))
       (else (MVvariable name (list (class-method-env (MVvariable (instance-class-name instance) state class-env instance)) (last-layer state)) class-env instance)))))
@@ -615,6 +617,11 @@
       ((declared? variable (namelist (top-layer state))) #t)
       (else (var-in-state? variable (remove-layer state))))))
 
+; return true if variable is in the class method list
+(define var-in-class-method?
+  (lambda (variable class-env)
+    (declared? variable (namelist (class-method-env class-env)))))
+
 ; return true if variable is in the static field list
 (define var-in-static?
   (lambda (variable class-env)
@@ -631,39 +638,39 @@
     (if (eq? (interpret filename classname) expected-value) (list filename 'passed)
         (error "Error:" filename 'expected expected-value 'but 'was 'returned (interpret filename classname)))))
 
-(test "4test1.txt" 'A 10) ; 10
-(test "4test2.txt" 'A 'true) ; true
-(test "4test3.txt" 'A 30) ; 30
-(test "4test4.txt" 'A 'false) ; false
-(test "4test5.txt" 'A 30) ; 30
-(test "4test5.txt" 'B 510) ; 510
-(test "4test6.txt" 'A 30) ; 30
-(test "4test6.txt" 'B 530) ; 530
-(test "4test7.txt" 'A 105) ; 105
-(test "4test7.txt" 'B 1155) ; 1155
-(test "4test8.txt" 'B 615) ; 615
-;(interpret "4test9.txt" 'B) ; ERROR: variable not found: d
-(test "4test9.txt" 'C 4321) ; 4321
-(test "4test10.txt" 'Square 400) ; 400
-(test "4test11.txt" 'A 15) ; 15
-(test "4test12.txt" 'A 125) ; 125
-(test "4test13.txt" 'A 100) ; 100
-(test "4test14.txt" 'A 2000400)
-(test "4test15.txt" 'Pow 64) ; 64
-
-(test "5test1.txt" 'A 20) ; 20
-(test "5test2.txt" 'Square 400) ; 400
-(test "5test3.txt" 'B 530) ; 530
-(test "5test4.txt" 'B 615) ; 615
-;(test "5test5.txt" 'C -716) ; -716
-(test "5test6.txt" 'A 15) ; 15
-(test "5test7.txt" 'A 12) ; 12
-(test "5test8.txt" 'A 110) ; 110
-(test "5test9.txt" 'A 125) ; 125
-(test "5test10.txt" 'A 36) ; 36
-(test "5test11.txt" 'A 54) ; 54
-(test "5test12.txt" 'C 26)
-(test "5test13.txt" 'Square 117)
+;(test "4test1.txt" 'A 10) ; 10
+;(test "4test2.txt" 'A 'true) ; true
+;(test "4test3.txt" 'A 30) ; 30
+;(test "4test4.txt" 'A 'false) ; false
+;(test "4test5.txt" 'A 30) ; 30
+;(test "4test5.txt" 'B 510) ; 510
+;(test "4test6.txt" 'A 30) ; 30
+;(test "4test6.txt" 'B 530) ; 530
+;(test "4test7.txt" 'A 105) ; 105
+;(test "4test7.txt" 'B 1155) ; 1155
+;(test "4test8.txt" 'B 615) ; 615
+;;(interpret "4test9.txt" 'B) ; ERROR: variable not found: d
+;(test "4test9.txt" 'C 4321) ; 4321
+;(test "4test10.txt" 'Square 400) ; 400
+;(test "4test11.txt" 'A 15) ; 15
+;(test "4test12.txt" 'A 125) ; 125
+;(test "4test13.txt" 'A 100) ; 100
+;(test "4test14.txt" 'A 2000400)
+;(test "4test15.txt" 'Pow 64) ; 64
+;
+;(test "5test1.txt" 'A 20) ; 20
+;(test "5test2.txt" 'Square 400) ; 400
+;(test "5test3.txt" 'B 530) ; 530
+;(test "5test4.txt" 'B 615) ; 615
+;;(test "5test5.txt" 'C -716) ; -716
+;(test "5test6.txt" 'A 15) ; 15
+;(test "5test7.txt" 'A 12) ; 12
+;(test "5test8.txt" 'A 110) ; 110
+;(test "5test9.txt" 'A 125) ; 125
+;(test "5test10.txt" 'A 36) ; 36
+;(test "5test11.txt" 'A 54) ; 54
+;(test "5test12.txt" 'C 26)
+;(test "5test13.txt" 'Square 117)
 (test "5test14.txt" 'Square 32)
 (test "5test15.txt" 'List 15)
 (test "5test16.txt" 'Box 16)
